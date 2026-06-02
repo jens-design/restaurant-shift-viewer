272ead2f0e10380ce160f103eb4c50577fad49f2
cterhungga26042006 <cterhungga2006@gmail.com>
2026-06-02 08:21:59 +0000

    hello

--- deleta.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/database.php';
-
-$id = $_GET['id'];
-$stmt = $pdo->prepare("DELETE FROM shifts WHERE id = ?");
-$stmt->execute([$id]);
-header('Location: index.php');
-exit;
-?>
